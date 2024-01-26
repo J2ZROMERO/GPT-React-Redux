8dd4e91310fa92d51e34f4eba8231e3e5956fad8
J2ZROMERO <jose-zromero@hotmail.com>
2024-01-26 15:53:13 -0600

    Update viewport

--- src/app/page.js
+++ src/app/page.js
@@ -65,67 +65,85 @@ export default function Home() {
   );
 
   return (
-    <div  className="app  container fluid">
-      <div className="row" >
-        
-      <div  className="side-bar d-flex flex-column container col">
-        <Button variant="outline-info mt-3 w-100" onClick={createNewChat}>
-          + New Chat
-        </Button>
-        <ListGroup className="history mt-4 h-100">
-          {uniqueTitles?.map((uniqueTitle, index) => (
-            <ListGroup.Item
-              key={index}
-              action
-              onClick={() => handleClick(uniqueTitle)}
-            >
-              {" "}
-              {uniqueTitle}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-        <Navbar>
-          <p>Made by Jose Zepeda</p>
-        </Navbar>
-      </div>
-      
-      <div class="col">
-      <div  className="main container">
-        <ListGroup className="feed">
-          {!currentTitle && <h1 className="nameTitle"> Jose-GPT</h1>}
-          <div className="chatContainer mt-3">
-            {currentChat?.map((chatMessage, index) => (
-              <ListGroup.Item className="mt-1" key={index} action>
-                <p>{chatMessage.role}</p>
-                <p className="">{chatMessage.content}</p>
+    <div className="app  container-fluid ">
+      <Navbar expand="md" bg="light" className="d-block d-md-none">
+        <Container fluid>
+          <Button
+            className="mt-3 w-100 "
+            variant="outline-info"
+            onClick={createNewChat}
+          >
+            + New Chat
+          </Button>
+          <Navbar.Collapse id="navbarNav">
+            <ul className="navbar-nav">
+              {/* Add your other Navbar links if needed */}
+            </ul>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="row vh-100">
+        <div className="side-bar d-flex flex-column container col-3 vh-100 d-none d-md-block">
+          <Button
+            className="mt-3 w-100 "
+            variant="outline-info"
+            onClick={createNewChat}
+          >
+            + New Chat
+          </Button>
+          <ListGroup className="history mt-4">
+            {uniqueTitles?.map((uniqueTitle, index) => (
+              <ListGroup.Item
+                key={index}
+                action
+                onClick={() => handleClick(uniqueTitle)}
+              >
+                {" "}
+                {uniqueTitle}
               </ListGroup.Item>
             ))}
-          </div>
-        </ListGroup>
-        <div className="bottom-section container">                       
-          <div className="input-container p-0 container">
-            <div className="submit ml-0 mr-0 p-0 container">
-              <textarea
-                rows={1}
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-              />{" "}
-              <h5 className="submit-icon" onClick={getMessages}>
-                {" "}
-                &#62;{" "}
-              </h5>
-            </div>
+          </ListGroup>
+          <Container className="m-0">
+            <p className="text-center">Made by Jose Zepeda</p>
+          </Container>
+        </div>
 
-            <p className="info">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            </p>
+        <div class="col-9">
+          <div className="main container vh-100 d-flex flex-column justify-content-between align-items-center">
+            <ListGroup className="feed">
+              {!currentTitle && <h1 className="nameTitle"> Jose-GPT</h1>}
+              <div className="chatContainer mt-3">
+                {currentChat?.map((chatMessage, index) => (
+                  <ListGroup.Item className="mt-1" key={index} action>
+                    <p>{chatMessage.role}</p>
+                    <p className="">{chatMessage.content}</p>
+                  </ListGroup.Item>
+                ))}
+              </div>
+            </ListGroup>
+            <div className="bottom-section container">
+              <div className="input-container p-0 container">
+                <div className="submit ml-0 mr-0 p-0 container d-flex justify-content-center">
+                  <textarea
+                    rows={1}
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                  />{" "}
+                  <h5 className="submit-icon" onClick={getMessages}>
+                    {" "}
+                    &#62;{" "}
+                  </h5>
+                </div>
+
+                <p className="info text-center">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      
-      
-    </div>
-
       </div>
     </div>
   );
